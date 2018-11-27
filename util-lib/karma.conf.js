22d1885c0a9b57519e3511b636c39189a780f39e
Rodrigo Silveira <Rodrigo.Silveira@madronesoft.com>
2018-11-26 18:24:41 -0800

    webpack Checked in the files that make stuff work

--- util-lib/karma.conf.js
+++ util-lib/karma.conf.js
@@ -1,72 +1,46 @@
-/*
- * Copyright 2018 Rodrigo Silveira
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-let webpackConfig = require('./webpack.dev.js');
-webpackConfig.entry = undefined;
-
 module.exports = function(config) {
     config.set({
-        basePath: process.env['INIT_CWD'],
-        frameworks: ['mocha', 'chai', 'karma-typescript', 'sinon'],
-        browsers: ['ChromeHeadless'],
+
+        frameworks: ["mocha", "karma-typescript"],
+
         files: [
-            {
-                pattern: 'test/util-class.test.ts',
-                watched: false,
-            }
+            { pattern: "node_modules/expect.js/index.js" },
+            { pattern: "test/**/*.spec.ts" },
+            { pattern: "src/**/*.ts" }
         ],
-        reporters: ['mocha'],
-        singleRun: true,
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: false,
-        concurrency: Infinity,
+
         preprocessors: {
-            '**/*.ts': ['karma-typescript']
+            "**/*.ts": ["karma-typescript"]
         },
+
+        reporters: ["dots", "karma-typescript", "mocha"],
+
+        browsers: ["ChromeHeadless"],
+
         karmaTypescriptConfig: {
             compilerOptions: {
-                target: 'esnext',
-                lib: ['dom', 'esnext'],
+                module: "commonjs"
             },
-            tsconfig: "./tsconfig.json",
+            tsconfig: './tsconfig.json'
         },
-        bundlerOptions: {
-            transforms: [require('karma-typescript-es6-transform')({ presets: 'env' })],
-        },
-        webpack: webpackConfig,
-        mochaReporter: {
-            colors: {
-                success: 'green',
-                info: 'bgGreen',
-                warning: 'cyan',
-                error: 'red'
-            },
-            symbols: {
-                success: '+',
-                info: '#',
-                warning: '!',
-                error: 'x'
-            }
-        },
+
+        logLevel: config.INFO,
+
+        singleRun: true,
+        
+		mochaReporter: {
+			colors: {
+				success: 'green',
+				info: 'bgGreen',
+				warning: 'cyan',
+				error: 'red'
+			},
+			symbols: {
+				success: '+',
+				info: '#',
+				warning: '!',
+				error: 'x'
+			}
+		}
     });
 };
